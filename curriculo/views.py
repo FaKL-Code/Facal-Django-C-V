ced68dc665a9b09eead3427c4cbdc951b8295e89
Julio Facal <starlyn.dev@gmail.com>
2022-11-04 11:39:23 -0300

    apontando para a api hospedada em vm ec2

--- curriculo/views.py
+++ curriculo/views.py
@@ -4,7 +4,7 @@ import requests
 import json
 
 def index(request):
-    url = 'http://127.0.0.1:8000/certificados'
+    url = 'http://ec2-18-219-40-14.us-east-2.compute.amazonaws.com:8000/certificados/'
     username = 'consumo'
     password = 'certificados'
     response = requests.get(url=url, auth=(username, password))
