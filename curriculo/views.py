673592ff1ef8ce91f31a0a102d2b7513218aca70
Julio Facal <starlyn.dev@gmail.com>
2022-11-03 22:09:36 -0300

    comecando as paginas

--- curriculo/views.py
+++ curriculo/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import requests
+import json
 
 def index(request):
+    url = 'http://127.0.0.1:8000/certificados'
+    username = 'consumo'
+    password = 'certificados'
+    response = requests.get(url=url, auth=(username, password))
+    cursos = response.json()
+    
+    print(cursos)
+    print(type(cursos))
+    
     return render(request, 'curriculo/index.html')
