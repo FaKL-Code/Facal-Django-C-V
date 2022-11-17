9f657c8d08b5020bc831332809cc07d1a7b5d525
Julio Facal <starlyn.dev@gmail.com>
2022-11-16 22:52:47 -0300

    teste dominio

--- curriculo/views.py
+++ curriculo/views.py
@@ -1,7 +1,6 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-import requests
-import json
+from django.test import Client
+from django.urls import reverse
+
 
 def index(request):
     url = 'http://ec2-18-219-40-14.us-east-2.compute.amazonaws.com:8000/certificados/'
@@ -9,8 +8,11 @@ def index(request):
     password = 'certificados'
     #response = requests.get(url=url, auth=(username, password))
     #cursos = response.json()
-    
-    #print(cursos)
-    #print(type(cursos))
-    
-    return render(request, 'curriculo/index.html')
+
+    # print(cursos)
+    # print(type(cursos))
+
+    client = Client(HTTP_HOST="www.example-a.dev")
+    view = reverse("index")
+    response = client.get(view)
+    request.assertEqual(response.status_code, 200)
